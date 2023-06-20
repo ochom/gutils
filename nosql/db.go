94ef853a2c7097c164ffd7274d0d1b879ccba664
Richard Ochom <ochomrichard752@gmail.com>
2023-06-20 20:36:15 +0300

    update mapping tables to names

--- nosql/db.go
+++ nosql/db.go
@@ -13,6 +13,36 @@ import (
 
 var conn *mongo.Database
 
+// GetTableName returns collection name in lower case separated by underscore
+// e.g User -> users, UserGroup -> user_groups
+func GetTableName(name string) string {
+	if name == "" {
+		panic("empty collection name")
+	}
+
+	var tableName string
+	for i, r := range name {
+		if i > 0 && 'A' <= r && r <= 'Z' {
+			tableName += "_"
+		}
+		tableName += string(r)
+	}
+
+	// convert to lower case
+	tableName = strings.ToLower(tableName)
+
+	// pluralize
+	if tableName[len(tableName)-1] == 'y' {
+		tableName = tableName[:len(tableName)-1] + "ies"
+	}
+
+	if tableName[len(tableName)-1] != 's' {
+		tableName += "s"
+	}
+
+	return tableName
+}
+
 // Init initializes the database connection with MongoDB
 func Init(dsn, dbName string) error {
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dsn))
@@ -39,20 +69,7 @@ func Col(v any) *mongo.Collection {
 		name = t.Name()
 	}
 
-	if strings.HasSuffix(name, "y") {
-		name = strings.TrimSuffix(name, "y") + "ies"
-	} else {
-		name += "s"
-	}
-
-	// to lowercase
-	name = strings.ToLower(name)
-
-	if name == "" {
-		panic("empty collection name")
-	}
-
-	return conn.Collection(name)
+	return conn.Collection(GetTableName(name))
 }
 
 func getIDField(v interface{}) (id string, err error) {
