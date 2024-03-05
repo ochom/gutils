d3b156842e93cdb65e0f22900c6657c48c6b80f1
ochom <ochomrichard752@gmail.com>
2024-03-05 19:38:04 +0300

    update uuid string

--- gttp/types.go
+++ gttp/types.go
@@ -3,5 +3,5 @@ package gttp
 // M  is a map[string]string
 type M map[string]string
 
-// Map is a map[string]interface{}
-type Map map[string]interface{}
+// Map is a map[string]any{}
+type Map map[string]any
