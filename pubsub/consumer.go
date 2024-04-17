012b0d3a869f36632679b83ac999344e1b913fd7
ochom <ochomrichard752@gmail.com>
2024-04-17 12:51:42 +0300

    fix consumer config

--- pubsub/consumer.go
+++ pubsub/consumer.go
@@ -4,25 +4,36 @@ import "fmt"
 
 // Consumer ...
 type Consumer struct {
-	url          string
-	exchange     string
-	queue        string
-	exchangeType string
-	tag          string
+	url      string
+	exchange string
+	queue    string
+	config   Config
 }
 
 type Config struct {
-	ExchangeType ExchangeType
-	ConsumerTag  string
+	Type      ExchangeType
+	Tag       string
+	AutoAck   bool
+	Exclusive bool
+	NoLocal   bool
+	NoWait    bool
+}
+
+var defaultConfig = Config{
+	Type:      Direct,
+	Tag:       "",
+	AutoAck:   true,
+	Exclusive: false,
+	NoLocal:   false,
+	NoWait:    false,
 }
 
 // Create a new consumer instance
 func NewConsumer(rabbitURL, exchange, queue string, config ...Config) *Consumer {
-	c := Consumer{url: rabbitURL, exchange: exchange, queue: queue, exchangeType: string(Direct)}
+	c := Consumer{url: rabbitURL, exchange: exchange, queue: queue, config: defaultConfig}
 
 	if len(config) > 0 {
-		c.exchangeType = string(config[0].ExchangeType)
-		c.tag = config[0].ConsumerTag
+		c.config = config[0]
 	}
 
 	return &c
@@ -37,18 +48,18 @@ func (c *Consumer) Consume(workerFunc func([]byte)) error {
 	defer ch.Close()
 	defer conn.Close()
 
-	if err := initPubSub(ch, c.exchange, c.queue, c.exchangeType); err != nil {
+	if err := initPubSub(ch, c.exchange, c.queue, string(c.config.Type)); err != nil {
 		return fmt.Errorf("failed to initialize a pubsub: %s", err.Error())
 	}
 
 	deliveries, err := ch.Consume(
-		c.queue, // queue
-		c.tag,   // consumerTag
-		true,    // auto-ack
-		false,   // exclusive
-		false,   // no-local
-		false,   // no-wait
-		nil,     // args
+		c.queue,            // queue
+		c.config.Tag,       // consumerTag
+		c.config.AutoAck,   // auto-ack
+		c.config.Exclusive, // exclusive
+		c.config.NoLocal,   // no-local
+		c.config.NoWait,    // no-wait
+		nil,                // args
 	)
 
 	if err != nil {
