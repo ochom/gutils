9184a60d9cd2b826f5d88dbadab3e19b22a8764a
ochom <ochomrichard752@gmail.com>
2024-05-24 17:10:56 +0300

    update read database

--- sql/common.go
+++ sql/common.go
@@ -7,17 +7,17 @@ import (
 
 // Create ...
 func Create[T any](data *T) error {
-	return conn.Create(data).Error
+	return connection.Create(data).Error
 }
 
 // Update ...
 func Update[T any](data *T) error {
-	return conn.Save(data).Error
+	return connection.Save(data).Error
 }
 
 // Delete ...
 func Delete[T any](scopes ...func(*gorm.DB) *gorm.DB) error {
-	return conn.Scopes(scopes...).Delete(new(T)).Error
+	return connection.Scopes(scopes...).Delete(new(T)).Error
 }
 
 // DeleteById ...
@@ -30,7 +30,7 @@ func DeleteById[T any](id any, scopes ...func(*gorm.DB) *gorm.DB) error {
 // FindOne ...
 func FindOne[T any](scopes ...func(*gorm.DB) *gorm.DB) (*T, error) {
 	var data T
-	if err := conn.Scopes(scopes...).First(&data).Error; err != nil {
+	if err := connection.Scopes(scopes...).First(&data).Error; err != nil {
 		return nil, err
 	}
 
@@ -47,7 +47,7 @@ func FindOneById[T any](id any, scopes ...func(*gorm.DB) *gorm.DB) (*T, error) {
 // FindAll ...
 func FindAll[T any](scopes ...func(*gorm.DB) *gorm.DB) []*T {
 	data := []*T{}
-	if err := conn.Scopes(scopes...).Find(&data).Error; err != nil {
+	if err := connection.Scopes(scopes...).Find(&data).Error; err != nil {
 		logs.Info("FindAll: %s", err.Error())
 		return []*T{}
 	}
@@ -58,7 +58,7 @@ func FindAll[T any](scopes ...func(*gorm.DB) *gorm.DB) []*T {
 // FindWithLimit ...
 func FindWithLimit[T any](page, limit int, scopes ...func(*gorm.DB) *gorm.DB) []*T {
 	data := []*T{}
-	if err := conn.Scopes(scopes...).Offset((page - 1) * limit).Limit(limit).Find(&data).Error; err != nil {
+	if err := connection.Scopes(scopes...).Offset((page - 1) * limit).Limit(limit).Find(&data).Error; err != nil {
 		logs.Info("FindWithLimit: %s", err.Error())
 		return []*T{}
 	}
@@ -69,7 +69,7 @@ func FindWithLimit[T any](page, limit int, scopes ...func(*gorm.DB) *gorm.DB) []
 func Count[T any](scopes ...func(*gorm.DB) *gorm.DB) int {
 	var count int64
 	var model T
-	if err := conn.Model(&model).Scopes(scopes...).Count(&count).Error; err != nil {
+	if err := connection.Model(&model).Scopes(scopes...).Count(&count).Error; err != nil {
 		logs.Info("Count: %s", err.Error())
 		return 0
 	}
@@ -80,7 +80,7 @@ func Count[T any](scopes ...func(*gorm.DB) *gorm.DB) int {
 // CountByTableName ...
 func CountByTableName(tableName string, query string, args ...any) int {
 	var count int64
-	if err := conn.Table(tableName).Where(query, args...).Count(&count).Error; err != nil {
+	if err := connection.Table(tableName).Where(query, args...).Count(&count).Error; err != nil {
 		logs.Info("Count: %s", err.Error())
 		return 0
 	}
--- sql/db.go
+++ sql/db.go
@@ -10,11 +10,11 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-// conn initialized in Init() and used in all other functions
-var conn *gorm.DB
+// n initialized in Init() and used in all other functions
+var connection *gorm.DB
 
 // Conn returns the database connection
-func Conn() *gorm.DB { return conn }
+func Conn() *gorm.DB { return connection }
 
 // defaultConfig ...
 var config = &Config{
@@ -54,31 +54,67 @@ func Init(configs ...*Config) (err error) {
 		}
 	}
 
+	return createInstance()
+}
+
+func createInstance() error {
 	switch config.DatabaseType {
 	case Postgres:
-		conn, err = gorm.Open(postgres.Open(config.Url), &gorm.Config{
-			Logger: logger.Default.LogMode(config.LogLevel),
-		})
+		return createPgInstance()
 	case MySQL:
-		conn, err = gorm.Open(mysql.Open(config.Url), &gorm.Config{
-			Logger: logger.Default.LogMode(config.LogLevel),
-		})
+		return createMysqlInstance()
 	default:
-		// - Set WAL mode (not strictly necessary each time because it's persisted in the database, but good for first run)
-		// - Set busy timeout, so concurrent writers wait on each other instead of erroring immediately
-		// - Enable foreign key checks
-		// -  see https://www.golang.dk/articles/go-and-sqlite-in-the-cloud
-		url := config.Url + "?_journal=WAL&_timeout=5000&_fk=true"
-		conn, err = gorm.Open(sqlite.Open(url), &gorm.Config{
-			Logger: logger.Default.LogMode(config.LogLevel),
-		})
+		return createSqliteInstance()
 	}
+}
+
+func createPgInstance() error {
+	conn, err := gorm.Open(postgres.Open(config.Url), &gorm.Config{
+		Logger: logger.Default.LogMode(config.LogLevel),
+	})
 
 	if err != nil {
 		return err
 	}
 
-	sqlDB, err := conn.DB()
+	connection = conn
+	return createPool()
+}
+
+func createMysqlInstance() error {
+	conn, err := gorm.Open(mysql.Open(config.Url), &gorm.Config{
+		Logger: logger.Default.LogMode(config.LogLevel),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	connection = conn
+	return createPool()
+}
+
+func createSqliteInstance() error {
+	// - Set WAL mode (not strictly necessary each time because it's persisted in the database, but good for first run)
+	// - Set busy timeout, so concurrent writers wait on each other instead of erroring immediately
+	// - Enable foreign key checks
+	// -  see https://www.golang.dk/articles/go-and-sqlite-in-the-cloud
+
+	url := config.Url + "?_journal=WAL&_timeout=5000&_fk=true"
+	conn, err := gorm.Open(sqlite.Open(url), &gorm.Config{
+		Logger: logger.Default.LogMode(config.LogLevel),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	connection = conn
+	return createPool()
+}
+
+func createPool() error {
+	sqlDB, err := connection.DB()
 	if err != nil {
 		return err
 	}
