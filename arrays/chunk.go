1dec04528a5996911ac28bc5608833ff1f41ab3d
ochom <ochomrichard752@gmail.com>
2024-08-12 21:33:16 +0300

    update array

--- arrays/chunk.go
+++ arrays/chunk.go
@@ -3,6 +3,10 @@ package arrays
 // Chunk will take a slice of any kind and a chunk size and return a slice of slices
 func Chunk[T any](arr []T, chunkSize int) [][]T {
 	chunks := [][]T{}
+	if len(arr) == 0 {
+		return chunks
+	}
+
 	for i := 0; i < len(arr); i += chunkSize {
 		end := i + chunkSize
 		if end > len(arr) {
