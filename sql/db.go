15b15a8241f7acac49c5ebd867ead539130e8782
ochom <ochomrichard752@gmail.com>
2024-10-04 17:21:12 +0300

    add new database implementation

--- sql/db.go
+++ sql/db.go
@@ -17,7 +17,7 @@ var db *gorm.DB
 func Conn() *gorm.DB { return db }
 
 // defaultConfig ...
-var config = &Config{
+var defaultConfig = Config{
 	Driver:       Sqlite,
 	Url:          "gorm.db",
 	LogLevel:     logger.Silent,
@@ -28,6 +28,24 @@ var config = &Config{
 
 // New initializes the database db with GORM
 func New(configs ...*Config) (err error) {
+	config := parseConfig(configs...)
+	newDB, err := createInstance(config)
+	if err != nil {
+		return err
+	}
+
+	db = newDB
+	return nil
+}
+
+// Create connection create and returns a new connection
+func CreateConnection(cfg ...*Config) (*gorm.DB, error) {
+	config := parseConfig(cfg...)
+	return createInstance(config)
+}
+
+func parseConfig(configs ...*Config) *Config {
+	config := &defaultConfig
 	for _, cfg := range configs {
 		if cfg.Driver != Sqlite {
 			config.Driver = cfg.Driver
@@ -54,71 +72,71 @@ func New(configs ...*Config) (err error) {
 		}
 	}
 
-	return createInstance()
+	return config
 }
 
-func createInstance() error {
+func createInstance(config *Config) (*gorm.DB, error) {
 	switch config.Driver {
 	case Postgres:
-		return createPgInstance()
+		return createPgInstance(config)
 	case MySQL:
-		return createMysqlInstance()
+		return createMysqlInstance(config)
 	default:
-		return createSqliteInstance()
+		return createSqliteInstance(config)
 	}
 }
 
-func createPgInstance() (err error) {
-	db, err = gorm.Open(postgres.Open(config.Url), &gorm.Config{
+func createPgInstance(config *Config) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(config.Url), &gorm.Config{
 		Logger: logger.Default.LogMode(config.LogLevel),
 	})
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	return createPool()
+	return createPool(db, config)
 }
 
-func createMysqlInstance() (err error) {
-	db, err = gorm.Open(mysql.Open(config.Url), &gorm.Config{
+func createMysqlInstance(config *Config) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(config.Url), &gorm.Config{
 		Logger: logger.Default.LogMode(config.LogLevel),
 	})
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	return createPool()
+	return createPool(db, config)
 }
 
-func createSqliteInstance() (err error) {
+func createSqliteInstance(config *Config) (*gorm.DB, error) {
 	// - Set WAL mode (not strictly necessary each time because it's persisted in the database, but good for first run)
 	// - Set busy timeout, so concurrent writers wait on each other instead of erroring immediately
 	// - Enable foreign key checks
 	// -  see https://www.golang.dk/articles/go-and-sqlite-in-the-cloud
 
 	url := config.Url + "?_journal=WAL&_timeout=5000&_fk=true"
-	db, err = gorm.Open(sqlite.Open(url), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open(url), &gorm.Config{
 		Logger: logger.Default.LogMode(config.LogLevel),
 	})
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	return createPool()
+	return createPool(db, config)
 }
 
-func createPool() error {
+func createPool(db *gorm.DB, config *Config) (*gorm.DB, error) {
 	sqlDB, err := db.DB()
 	if err != nil {
-		return err
+		return db, err
 	}
 
 	sqlDB.SetMaxIdleConns(config.MaxIdleConns)
 	sqlDB.SetMaxOpenConns(config.MaxOpenConns)
 	sqlDB.SetConnMaxLifetime(config.ConnLifeTime)
 
-	return nil
+	return db, nil
 }
