64d95224ca271b3340b279b9703985ded38183f9
ochom <ochomrichard752@gmail.com>
2025-01-30 18:34:39 +0300

    fix: set timeouts in fiber

--- gttp/gofiber.go
+++ gttp/gofiber.go
@@ -28,31 +28,6 @@ func (c *fiberClient) sendRequest(url, method string, headers M, body []byte, ti
 		timeout = timeouts[0]
 	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	// defer cancel()
-
-	// result := make(chan *Response, 1)
-	// go func() {
-	// 	resp := c.makeRequest(url, method, headers, body)
-	// 	result <- resp
-	// }()
-
-	// select {
-	// case <-ctx.Done():
-	// 	return nil, ctx.Err()
-	// case r := <-result:
-	// 	if len(r.Errors) == 0 {
-	// 		return r, nil
-	// 	}
-
-	// 	errStrings := []string{}
-	// 	for _, err := range r.Errors {
-	// 		errStrings = append(errStrings, err.Error())
-	// 	}
-
-	// 	return r, errors.New(strings.Join(errStrings, ", "))
-	// }
-
 	resp := c.makeRequest(url, method, headers, body, timeout)
 	if len(resp.Errors) == 0 {
 		return resp, nil
@@ -67,37 +42,40 @@ func (c *fiberClient) sendRequest(url, method string, headers M, body []byte, ti
 }
 
 // makeRequest sends a request to the specified URL.
-func (c *fiberClient) makeRequest(url, method string, headers M, body []byte, timeout time.Duration) (resp *Response) {
-	var req *fiber.Agent
+func (c *fiberClient) makeRequest(url, method string, headers M, body []byte, timeout time.Duration) *Response {
+	var agent *fiber.Agent
 	switch method {
 	case "POST":
-		req = fiber.Post(url)
+		agent = fiber.Post(url)
 	case "GET":
-		req = fiber.Get(url)
+		agent = fiber.Get(url)
 	case "DELETE":
-		req = fiber.Delete(url)
+		agent = fiber.Delete(url)
 	case "PUT":
-		req = fiber.Put(url)
+		agent = fiber.Put(url)
 	case "PATCH":
-		req = fiber.Patch(url)
+		agent = fiber.Patch(url)
 	default:
 		err := fmt.Errorf("unknown method: %s", method)
 		return NewResponse(500, []error{err}, nil)
 	}
 
 	// skip ssl verification
-	req.InsecureSkipVerify()
-	req.Timeout(timeout)
+	agent.InsecureSkipVerify()
+	agent.Timeout(timeout)
 
+	// add request headers
 	for k, v := range headers {
-		req.Add(k, v)
+		agent.Add(k, v)
 	}
 
+	// add request body
 	if method == "POST" || method == "PUT" || method == "PATCH" {
-		req.Body(body)
+		agent.Body(body)
 	}
 
-	code, content, errs := req.Bytes()
+	// make request
+	code, content, errs := agent.Bytes()
 	if code == 0 {
 		code = 500
 	}
