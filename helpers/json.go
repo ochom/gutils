9f372d157970f4867a0481d15d51f7a6cf10db5c
Richard Ochom <ochomrichard752@gmail.com>
2025-07-11 08:56:31 +0300

    update json to bytes payload

--- helpers/json.go
+++ helpers/json.go
@@ -2,12 +2,45 @@ package helpers
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/ochom/gutils/logs"
 )
 
+type ByteData []byte
+
+// String converts byte data to string
+func (b ByteData) String() string {
+	if b == nil {
+		return ""
+	}
+	return string(b)
+}
+
 // ToBytes converts provided interface to slice of bytes
-func ToBytes[T any](payload T) []byte {
+func ToBytes(payload any) ByteData {
+	stringValue, ok := payload.(string)
+	if ok {
+		return ByteData(stringValue)
+	}
+
+	numericValue, ok := payload.(float64)
+	if ok {
+		return ByteData(fmt.Appendf(nil, "%f", numericValue))
+	}
+
+	boolValue, ok := payload.(bool)
+	if ok {
+		if boolValue {
+			return ByteData("true")
+		}
+		return ByteData("false")
+	}
+
+	if payload == nil {
+		return nil
+	}
+
 	bytesPayload, err := json.Marshal(&payload)
 	if err != nil {
 		logs.Error("Failed to marshal JSON: %s", err.Error())
