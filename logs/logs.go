009976106cb01096596077fae7b915fe7a8e57bf
Richard Ochom <ochomrichard752@gmail.com>
2026-02-20 15:29:09 +0300

    add customization on loggers

--- logs/logs.go
+++ logs/logs.go
@@ -2,7 +2,6 @@ package logs
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"runtime"
@@ -16,18 +15,48 @@ const (
 	ColorBlue   = "\033[34m"
 )
 
-var c *log.Logger
+// LogLevel ...
+type LogLevel int
+
+// LogLevels ...
+const (
+	DebugLevel LogLevel = iota
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+)
+
+// Config ...
+type Config struct {
+	Loggers       map[LogLevel]*log.Logger
+	DefaultLogger *log.Logger
+}
+
+var defaultConfig = Config{
+	Loggers:       map[LogLevel]*log.Logger{},
+	DefaultLogger: log.New(os.Stdout, "", log.LstdFlags),
+}
+
+var logger *Config
 
 func init() {
-	c = log.New(os.Stdout, "", log.LstdFlags)
+	logger = &defaultConfig
 }
 
-// SetOutput ...
-func SetOutput(w io.Writer) {
-	c.SetOutput(w)
+// InitLogger ...
+func InitLogger(config ...Config) {
+	var cfg Config
+	if len(config) == 0 {
+		cfg = config[0]
+	} else {
+		cfg = defaultConfig
+	}
+
+	logger = &cfg
 }
 
-func print(s string) {
+func print(l LogLevel, s string) {
 	_, file, line, ok := runtime.Caller(2)
 	if ok {
 		s = fmt.Sprintf("%s:%d %s", file, line, s)
@@ -35,31 +64,35 @@ func print(s string) {
 		s = fmt.Sprintf("%s %s", file, s)
 	}
 
-	c.Println(s)
+	if log, ok := logger.Loggers[l]; ok {
+		log.Println(s)
+	} else {
+		logger.DefaultLogger.Println(s)
+	}
 }
 
 // Debug ...
 func Debug(format string, args ...any) {
-	print(fmt.Sprintf("%sDEBUG%s: %s", ColorBlue, ColorReset, fmt.Sprintf(format, args...)))
+	print(DebugLevel, fmt.Sprintf("%sDEBUG%s: %s", ColorBlue, ColorReset, fmt.Sprintf(format, args...)))
 }
 
 // Info ...
 func Info(format string, args ...any) {
-	print(fmt.Sprintf("%sINFO%s: %s", ColorGreen, ColorReset, fmt.Sprintf(format, args...)))
+	print(InfoLevel, fmt.Sprintf("%sINFO%s: %s", ColorGreen, ColorReset, fmt.Sprintf(format, args...)))
 }
 
 // Warn ...
 func Warn(format string, args ...any) {
-	print(fmt.Sprintf("%sWARN%s: %s", ColorYellow, ColorReset, fmt.Sprintf(format, args...)))
+	print(WarnLevel, fmt.Sprintf("%sWARN%s: %s", ColorYellow, ColorReset, fmt.Sprintf(format, args...)))
 }
 
 // Error ...
 func Error(format string, args ...any) {
-	print(fmt.Sprintf("%sERROR%s: %s", ColorRed, ColorReset, fmt.Sprintf(format, args...)))
+	print(ErrorLevel, fmt.Sprintf("%sERROR%s: %s", ColorRed, ColorReset, fmt.Sprintf(format, args...)))
 }
 
 // Fatal ...
 func Fatal(format string, args ...any) {
-	print(fmt.Sprintf("%sFATAL%s: %s", ColorRed, ColorReset, fmt.Sprintf(format, args...)))
+	print(FatalLevel, fmt.Sprintf("%sFATAL%s: %s", ColorRed, ColorReset, fmt.Sprintf(format, args...)))
 	os.Exit(1)
 }
