290dae53f447920c0996705ae86e77eec5e17270
Richard Ochom <ochomrichard752@gmail.com>
2026-06-06 12:27:47 +0300

    feat: add fetch to cache

--- cache/fetch.go
+++ cache/fetch.go
@@ -19,23 +19,33 @@ import (
 //	profile := cache.Fetch("user:42:profile", 5*time.Minute, func() Profile {
 //		return fetchProfileFromDB(42)
 //	})
-func Fetch[T any](key string, duration time.Duration, fetchFunc func() T) T {
-	fetchAndSet := func() []byte {
-		result := fetchFunc()
+func Fetch[T any](key string, duration time.Duration, fetchFunc func() (T, error)) (T, error) {
+	fetchAndSet := func() ([]byte, error) {
+		result, err := fetchFunc()
+		if err != nil {
+			var zero T
+			return jsonx.Encode(zero), err
+		}
 
 		jsonData := jsonx.Encode(result)
 		_ = Set(key, jsonData, duration)
-		return jsonData
+		return jsonData, nil
 	}
 
 	cached := Get(key)
 	if cached != nil { /*  */
 		go func() {
-			_ = fetchAndSet()
+			_, _ = fetchAndSet()
 		}()
 
-		return jsonx.Decode[T](cached)
+		return jsonx.Decode[T](cached), nil
 	}
 
-	return jsonx.Decode[T](fetchAndSet())
+	result, err := fetchAndSet()
+	if err != nil {
+		var zero T
+		return zero, err
+	}
+
+	return jsonx.Decode[T](result), nil
 }
